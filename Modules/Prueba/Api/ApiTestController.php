61d039ca6041b291c980711fdd4b3c962b0720f8
Rafael San José Tovar <rsanjose@alxarafe.com>
2024-11-22 18:57:10 +0100

    PHP version requirement increased to 8.2, documentation and other minor changes.

--- Modules/Prueba/Api/ApiTestController.php
+++ Modules/Prueba/Api/ApiTestController.php
@@ -1,12 +1,11 @@
 <?php
 
-/**
- * Copyright (C) 2024       Rafael San José             <rsanjose@alxarafe.com>
+/* Copyright (C) 2024       Rafael San José         <rsanjose@alxarafe.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
+ * any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,15 +16,21 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-use Alxarafe\Tools\Dispatcher;
+namespace Modules\Prueba\Api;
 
-const BASE_PATH = __DIR__;
+use Alxarafe\Base\Controller\ApiController;
 
-$autoload_filename = realpath(BASE_PATH . '/../vendor/autoload.php');
-if (!file_exists($autoload_filename)) {
-    die('<h1>COMPOSER ERROR</h1><p>You need to run: "composer install"</p>');
+class ApiTestController extends ApiController
+{
+    const MENU = 'public|bootstrap';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function doIndex(): bool
+    {
+        return true;
+    }
 }
-
-require_once $autoload_filename;
-
-Dispatcher::run();
